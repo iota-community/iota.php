6974b404404a1f34b31339c4cb33284dd1607106
SourCL <info@sourcl.com>
2021-05-10 18:40:48 +0200

    add example 98_client_fetch.php

---
+++ examples/98_client_fetch.php
@@ -0,0 +1,11 @@
+<?php
+  // include iota lib
+  require_once("../iota.php");
+  // create client
+  $_client = new iota('https://api.lb-0.testnet.chrysalis2.com');
+  // fetchStatus
+  $_result = $_client->fetchStatus('get', '/health');
+  // fetchJSON
+  $_result = $_client->fetchJSON('get', 'info');
+  // fetchArray
+  $_result = $_client->fetchArray('get', 'info');
