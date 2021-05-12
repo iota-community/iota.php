51fd4955d0dbcfeb0b9349ddfc81b42ba2547dee
SourCL <info@sourcl.com>
2021-05-12 18:21:57 +0200

    update client new functions output, address, peer, receipts, treasury

--- src/client.php
+++ src/client.php
@@ -195,4 +195,91 @@ abstract class client {
    * @return schemas\response\Peers
    */
   abstract public function peers(): \iota\schemas\response\Peers;
+
+  /**
+   * Add a given peer to the node.
+   *
+   * @param string $multiAddress
+   * @param string $alias
+   *
+   * @return \iota\schemas\response\AddPeer
+   */
+  abstract public function peerAdd(string $multiAddress, string $alias = null): \iota\schemas\response\AddPeer;
+
+  /**
+   * Remove/disconnect a given peer.
+   *
+   * @param string $peerId
+   *
+   * @return string
+   */
+  abstract public function peerDelete(string $peerId): void;
+
+  /**
+   * Find an output by its identifier.
+   *
+   * @param string $outputId Identifier of the output encoded in hex. An output is identified by the concatenation of transaction_id+output_index
+   *
+   * @return \iota\schemas\response\Output
+   */
+  abstract public function output(string $outputId): \iota\schemas\response\Output;
+
+  /**
+   * Find an output by its identifier.
+   *
+   * @param string $outputId Identifier of the output encoded in hex. An output is identified by the concatenation of transaction_id+output_index
+   *
+   * @return \iota\schemas\response\Output
+   */
+  abstract public function address(string $addressBech32): \iota\schemas\response\BalanceAddress;
+
+  /**
+   * Get all outputs that use a given bech32-encoded address
+   *
+   * @param string $addressBech32 bech32-encoded address that is referenced by the outputs.
+   * @param int    $type          Allows to filter the results by output type. Set to value 0 to filter outputs of type SigLockedSingleOutput. Set to value 1 to filter outputs of type SigLockedDustAllowanceOutput
+   * @param bool   $includeSpend  Set to true to also include the known spent outputs for the given address.
+   *
+   * @return \iota\schemas\response\OutputAddress
+   */
+  abstract public function addressesOutput(string $addressBech32, int $type = 0, bool $includeSpend = false): \iota\schemas\response\OutputAddress;
+
+  /**
+   * Get the balance of a hex-encoded Ed25519 address
+   *
+   * @param string $addressEd25519
+   *
+   * @return \iota\schemas\response\BalanceAddress
+   * @throws \Exception
+   */
+  abstract public function addressEd25519(string $addressEd25519): \iota\schemas\response\BalanceAddress;
+
+  /**
+   * Get all outputs that use a given hex-encoded Ed25519 address. If count equals maxResults, then there might be more outputs available but those were skipped for performance reasons. User should sweep the address to reduce the amount of outputs.
+   *
+   * @param string $addressEd25519 hex-encoded Ed25519 address that is referenced by the outputs.
+   * @param int    $type           Allows to filter the results by output type. Set to value 0 to filter outputs of type SigLockedSingleOutput. Set to value 1 to filter outputs of type SigLockedDustAllowanceOutput
+   * @param bool   $includeSpend   Set to true to also include the known spent outputs for the given address.
+   *
+   * @return \iota\schemas\response\OutputAddress
+   */
+  abstract public function addressesed25519Output(string $addressEd25519, int $type = 0, bool $includeSpend = false): \iota\schemas\response\OutputAddress;
+
+  /**
+   * Get all the stored receipts or those for a given migrated at index.
+   *
+   * @param int|null $migratedAt
+   *
+   * @return \iota\schemas\response\Receipts
+   * @throws \Exception
+   */
+  abstract public function receipts(int $migratedAt = null): \iota\schemas\response\Receipts;
+
+  /**
+   * Returns information about the treasury
+   *
+   * @return \iota\schemas\response\Receipts
+   * @throws \Exception
+   */
+  abstract public function treasury(): \iota\schemas\response\Treasury;
 }
--- src/client/singleNode.php
+++ src/client/singleNode.php
@@ -115,4 +115,107 @@ class singleNode extends \iota\client {
   public function peers(): \iota\schemas\response\Peers {
     return (new \iota\api\peers($this))->list();
   }
+
+  /**
+   * Add a given peer to the node.
+   *
+   * @param string $multiAddress
+   * @param string $alias
+   *
+   * @return \iota\schemas\response\AddPeer
+   */
+  public function peerAdd(string $multiAddress, string $alias = null): \iota\schemas\response\AddPeer {
+    return (new \iota\api\peers($this))->add($multiAddress, $alias);
+  }
+
+  /**
+   * Remove/disconnect a given peer.
+   *
+   * @param string $peerId
+   *
+   * @return string
+   */
+  public function peerDelete(string $peerId): void {
+    (new \iota\api\peers($this))->delete($peerId);
+  }
+
+  /**
+   * Find an output by its identifier.
+   *
+   * @param string $outputId Identifier of the output encoded in hex. An output is identified by the concatenation of transaction_id+output_index
+   *
+   * @return \iota\schemas\response\Output
+   */
+  public function output(string $outputId): \iota\schemas\response\Output {
+    return (new \iota\api\utxo($this))->find($outputId);
+  }
+
+  /**
+   * Find an output by its identifier.
+   *
+   * @param string $outputId Identifier of the output encoded in hex. An output is identified by the concatenation of transaction_id+output_index
+   *
+   * @return \iota\schemas\response\Output
+   */
+  public function address(string $addressBech32): \iota\schemas\response\BalanceAddress {
+    return (new \iota\api\utxo($this))->addresses($addressBech32);
+  }
+
+  /**
+   * @param string $addressBech32 bech32-encoded address that is referenced by the outputs.
+   * @param int    $type          Allows to filter the results by output type. Set to value 0 to filter outputs of type SigLockedSingleOutput. Set to value 1 to filter outputs of type SigLockedDustAllowanceOutput
+   * @param bool   $includeSpend  Set to true to also include the known spent outputs for the given address.
+   *
+   * @return \iota\schemas\response\OutputAddress
+   */
+  public function addressesOutput(string $addressBech32, int $type = 0, bool $includeSpend = false): \iota\schemas\response\OutputAddress {
+    return (new \iota\api\utxo($this))->addressesOutput($addressBech32, $type, $includeSpend);
+  }
+
+  /**
+   * Get the balance of a hex-encoded Ed25519 address
+   *
+   * @param string $addressEd25519
+   *
+   * @return \iota\schemas\response\BalanceAddress
+   * @throws \Exception
+   */
+  public function addressEd25519(string $addressEd25519): \iota\schemas\response\BalanceAddress {
+    return (new \iota\api\utxo($this))->addressEd25519($addressEd25519);
+  }
+
+  /**
+   * Get all outputs that use a given hex-encoded Ed25519 address. If count equals maxResults, then there might be more outputs available but those were skipped for performance reasons. User should sweep the address to reduce the amount of outputs.
+   *
+   * @param string $addressEd25519 hex-encoded Ed25519 address that is referenced by the outputs.
+   * @param int    $type           Allows to filter the results by output type. Set to value 0 to filter outputs of type SigLockedSingleOutput. Set to value 1 to filter outputs of type SigLockedDustAllowanceOutput
+   * @param bool   $includeSpend   Set to true to also include the known spent outputs for the given address.
+   *
+   * @return \iota\schemas\response\OutputAddress
+   */
+  public function addressesed25519Output(string $addressEd25519, int $type = 0, bool $includeSpend = false): \iota\schemas\response\OutputAddress {
+    return (new \iota\api\utxo($this))->addressesed25519Output($addressEd25519, $type, $includeSpend);
+  }
+
+  /**
+   * Get all the stored receipts or those for a given migrated at index.
+   *
+   * @param int|null $migratedAt
+   *
+   * @return \iota\schemas\response\Receipts
+   * @throws \Exception
+   */
+  public function receipts(int $migratedAt = null): \iota\schemas\response\Receipts {
+    return (new \iota\api\utxo($this))->receipts($migratedAt);
+  }
+
+  /**
+   * Returns information about the treasury
+   *
+   * @return \iota\schemas\response\Receipts
+   * @throws \Exception
+   */
+  public function treasury(): \iota\schemas\response\Treasury {
+    return (new \iota\api\utxo($this))->treasury();
+  }
 }
