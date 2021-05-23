9311e5806b6cf179ec57005e37ed760edbc4babf
SourCL <info@sourcl.com>
2021-05-23 08:19:34 +0200

    new functions

--- src/converter.php
+++ src/converter.php
@@ -6,58 +6,218 @@
  */
 class converter {
   /**
-   * Convert binary data into hexadecimal representation
+   * @param string $val
    *
-   * @param string $string <p>
-   *                       A character.
-   *                       </p>
-   *
-   * @return string the hexadecimal representation of the given string.
+   * @return string
    */
-  static public function strtohex(string $string): string {
-    return \bin2hex($string);
+  static public function bin2hex(string $val): string {
+    return \bin2hex($val);
   }
 
   /**
-   * Convert hexadecimal string to its binary representation.
-   * If the hexadecimal input string is of odd length or invalid hexadecimal string an <code>E_WARNING</code> level error is emitted.
+   * @param string $val
    *
-   * @param string $string Hexadecimal string to convert.
-   *
-   * @return string|false The binary representation of the given data or <b>FALSE</b> on failure.
+   * @return string|false
+   * @throws exception\converter
    */
-  static public function hextostr(string $string): string|false {
-    return \hex2bin($string);
+  static public function hex2bin(string $val): string|false {
+    if(!self::isHex($val)) {
+      throw new \iota\exception\converter("Input string must be hexadecimal string");
+    }
+
+    return \hex2bin($val);
   }
 
   /**
-   * Encodes data with MIME base64
+   * @param string $val
    *
-   * @param string $string <p>
-   *                       The data to encode.
-   *                       </p>
-   *
-   * @return string The encoded data, as a string.
+   * @return string
    */
-  static public function strtobase64(string $string): string {
-    return \base64_encode($string);
+  static public function bits2hex(string $val): string {
+    $_hex = "";
+    foreach(\str_split($val, 4) as $_chunk) {
+      $_hex .= \base_convert($_chunk, 2, 16);
+    }
+
+    return $_hex;
   }
 
   /**
-   * Decodes data encoded with MIME base64
+   * @param string $val
    *
-   * @param string $string <p>
-   *                       The encoded data.
-   *                       </p>
-   * @param bool   $strict [optional] <p>
-   *                       Returns false if input contains character from outside the base64
-   *                       alphabet.
-   *                       </p>
-   *
-   * @return string|false the original data or false on failure. The returned data may be
-   * binary.
+   * @return string
+   * @throws exception\converter
    */
-  static public function base64tostr(string $string, bool $strict = false): string|false {
-    return \base64_decode($string, $strict);
+  static public function hex2bits(string $val): string {
+    if(!self::isHex($val)) {
+      throw new \iota\exception\converter("Input string must be hexadecimal string");
+    }
+
+    $_bits = "";
+    for($i = 0; $i < \strlen($val); $i++) {
+      $_bits .= \str_pad(\base_convert($val[$i], 16, 2), 4, '0', STR_PAD_LEFT);
+    }
+
+    return $_bits;
+  }
+
+  /**
+   * @param string $val
+   *
+   * @return array
+   * @throws exception\converter
+   */
+  static public function hex2byteArray(string $val): array {
+    if(!self::isHex($val)) {
+      throw new \iota\exception\converter("Input string must be hexadecimal string");
+    }
+    $_bin = \hex2bin($val);
+
+    return \unpack('C*', $_bin);
+  }
+
+  /**
+   * @param array $data
+   *
+   * @return string
+   */
+  static public function byteArray2Hex(array $data): string {
+    $_chars = \array_map("chr", $data);
+    $_bin   = \join($_chars);
+
+    return \bin2hex($_bin);
+  }
+
+  /**
+   * @param string $val
+   *
+   * @return array
+   */
+  static function string2ByteArray(string $val): array {
+    return \unpack('C*', $val);
+  }
+
+  /**
+   * @param array $data
+   *
+   * @return string
+   */
+  static function byteArray2String(array $data): string {
+    $_chars = \array_map("chr", $data);
+
+    return \join($_chars);
+  }
+
+  /**
+   * @param string $val
+   *
+   * @return string
+   */
+  static public function base64_encode(string $val): string {
+    return \base64_encode($val);
+  }
+
+  /**
+   * @param string $val
+   * @param bool   $strict
+   *
+   * @return string|false
+   */
+  static public function base64_decode(string $val, bool $strict = false): string|false {
+    return \base64_decode($val, $strict);
+  }
+
+  /**
+   * @param array $data
+   * @param int   $inLen
+   * @param int   $fromBits
+   * @param int   $toBits
+   * @param bool  $_pad
+   *
+   * @return array
+   * @throws exception\converter
+   */
+  static public function bits(array $data, int $inLen, int $fromBits, int $toBits, $_pad = true):array {
+    $_acc    = 0;
+    $_bits   = 0;
+    $_ret    = [];
+    $_maxv   = (1 << $toBits) - 1;
+    $_maxacc = (1 << ($fromBits + $toBits - 1)) - 1;
+    for($_i = 0; $_i < $inLen; $_i++) {
+      $_value = $data[$_i];
+      if($_value < 0 || $_value >> $fromBits) {
+        throw new \Exception('Invalid value for convert bits');
+      }
+      $_acc  = (($_acc << $fromBits) | $_value) & $_maxacc;
+      $_bits += $fromBits;
+      while($_bits >= $toBits) {
+        $_bits  -= $toBits;
+        $_ret[] = (($_acc >> $_bits) & $_maxv);
+      }
+    }
+    if($_pad) {
+      if($_bits) {
+        $_ret[] = ($_acc << $toBits - $_bits) & $_maxv;
+      }
+    }
+    else if($_bits >= $fromBits || ((($_acc << ($toBits - $_bits))) & $_maxv)) {
+      throw new \iota\exception\converter('Invalid data');
+    }
+
+    return $_ret;
+  }
+
+  /**
+   * @param mixed $val
+   *
+   * @return bool
+   */
+  static public function isHex(mixed $val): bool {
+    return \is_string($val) && \preg_match('/^[a-z0-9+\/]+={0,2}$/i', $val);
+  }
+
+  /**
+   * @param mixed $val
+   *
+   * @return bool
+   */
+  static public function isNumeric(mixed $val): bool {
+    return is_numeric($val);
+  }
+
+  /**
+   * @param $val
+   *
+   * @return bool
+   */
+  public static function isUtf8(mixed $val): bool {
+    return \is_string($val) && \strlen($val) !== \mb_strlen($val);
+  }
+
+  /**
+   * @param $val
+   *
+   * @return bool
+   */
+  static public function isBitwise(mixed $val): bool {
+    return \is_string($val) && \preg_match('/^[01]+$/', $val);
+  }
+
+  /**
+   * @param mixed $val
+   *
+   * @return bool
+   */
+  static public function isBase64(mixed $val): bool {
+    return \is_string($val) && \preg_match('/^[a-z0-9+\/]+={0,2}$/i', $val);
+  }
+
+  /**
+   * @param mixed $val
+   *
+   * @return bool
+   */
+  static public function isBase16(mixed $val): bool {
+    return \is_string($val) && \preg_match('/^(0x)?[a-f0-9]+$/i', $val);
   }
 }
