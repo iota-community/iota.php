e6f1bdad1784e92690bb065e06081021b250f580
SourCL <info@sourcl.com>
2021-06-16 12:07:53 +0200

    add testTangleApi

--- tests/node.php
+++ tests/node.php
@@ -43,4 +43,17 @@
       $this->assertArrayHasKey('networkId', (array)$_result, 'result need key "networkId"');
       $this->assertArrayHasKey('bech32HRP', (array)$_result, 'result need key "bech32HRP"');
     }
+
+    /**
+     * @throws Exception
+     */
+    public function testTangleApi(): void {
+      $this->createApiClient();
+      $this->assertTrue($this->_client->health(), 'Health not OK');
+      //
+      $_result = $this->_client->tips();
+      $this->assertJson((string)$_result, 'client returned not a valid json');
+      $this->assertIsArray((array)$_result, 'client returned not a valid array');
+      $this->assertArrayHasKey('tipMessageIds', (array)$_result, 'result need key "tipMessageIds"');
+    }
   }
