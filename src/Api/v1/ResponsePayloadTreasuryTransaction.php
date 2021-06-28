83f99f88c4e44aa3a561d249ae7674492a6d3d2c
SourCL <info@sourcl.com>
2021-06-28 07:40:18 +0200

    add payload

---
+++ src/Api/v1/ResponsePayloadTreasuryTransaction.php
@@ -0,0 +1,32 @@
+<?php namespace IOTA\Api\v1;
+
+use IOTA\Models\AbstractApiResponse;
+
+/**
+ * Class ResponsePayloadMilestone
+ *
+ * @package      IOTA\Api\v1
+ * @author       StefanBraun @IOTAphp
+ * @copyright    Copyright (c) 2021, StefanBraun
+ */
+class ResponsePayloadTreasuryTransaction extends AbstractApiResponse {
+  /**
+   * @var int
+   */
+  public int $type = 4;
+  /**
+   * @var array
+   */
+  public array $input;
+  /**
+   * @var array
+   */
+  public array $output;
+
+  /**
+   *
+   */
+  protected function parse(): void {
+    $this->defaultParse();
+  }
+}
