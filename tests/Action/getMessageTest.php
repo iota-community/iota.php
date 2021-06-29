1572a8d6b7ba212a215f9ebb9785e37a1cca1572
SourCL <info@sourcl.com>
2021-06-29 12:08:11 +0200

    add phpUnit test

---
+++ tests/Action/getMessageTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+  require_once "./autoload.php";
+
+  use PHPUnit\Framework\TestCase;
+  use IOTA\Action\getMessage;
+  use IOTA\Action\sendMessage;
+  use IOTA\Client\SingleNodeClient;
+  use IOTA\Api\v1\ResponseMessage;
+
+  /**
+   * Class getMessageTest
+   *
+   * @author       StefanBraun @IOTAphp
+   * @copyright    Copyright (c) 2021, StefanBraun
+   */
+  final class getMessageTest extends TestCase {
+
+    /**
+     * @var SingleNodeClient
+     */
+    protected SingleNodeClient $client;
+
+
+    /**
+     *
+     */
+    public function setUp(): void {
+      $this->client = new SingleNodeClient();
+      $this->assertInstanceOf(SingleNodeClient::class, $this->client);
+    }
+
+    /**
+     *
+     */
+    public function testgetMessage() {
+      $msg = (new sendMessage($this->client))->index('#iota.php')
+                                             ->data('message test! follow me on Twitter @IOTAphp')->run();
+
+      $ret = (new getMessage($this->client))->messageId($msg->messageId);
+      $this->assertInstanceOf(getMessage::class, $ret);
+      $this->assertInstanceOf(ResponseMessage::class, $ret->getResult());
+    }
+  }
