7467fec6d0fcb154fa8e4d04d44cb4e40d78f642
SourCL <info@sourcl.com>
2021-07-24 14:14:05 +0200

    add Exception\Util class

---
+++ src/Exception/Util.php
@@ -0,0 +1,13 @@
+<?php namespace IOTA\Exception;
+use Exception;
+
+/**
+ * Class Util
+ *
+ * @package      IOTA\Exception
+ * @author       StefanBraun @IOTAphp
+ * @copyright    Copyright (c) 2021, StefanBraun
+ */
+class Util extends Exception {
+
+}
