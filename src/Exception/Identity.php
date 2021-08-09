b5a7f5ca8eac10b29d18aec04d93efba6bf2371c
SourCL <info@sourcl.com>
2021-08-09 08:25:59 +0200

    add Exception Identity

---
+++ src/Exception/Identity.php
@@ -0,0 +1,13 @@
+<?php namespace IOTA\Exception;
+use Exception;
+
+/**
+ * Class Identity
+ *
+ * @package      IOTA\Exception
+ * @author       StefanBraun @IOTAphp
+ * @copyright    Copyright (c) 2021, StefanBraun
+ */
+class Identity extends Exception {
+
+}
