be634028a0d3d2fecc482139eed74c1943b0f254
SourCL <info@sourcl.com>
2021-08-09 08:26:23 +0200

    add Model AbstractMap

---
+++ src/Models/AbstractMap.php
@@ -0,0 +1,37 @@
+<?php namespace IOTA\Models;
+
+use IOTA\Exception\Helper as ExceptionHelper;
+use IOTA\Helper\JSON;
+
+/**
+ * Class AbstractDid
+ *
+ * @package      IOTA\Models
+ * @author       StefanBraun @IOTAphp
+ * @copyright    Copyright (c) 2021, StefanBraun
+ */
+abstract class AbstractMap {
+  /**
+   * @return JSON
+   * @throws ExceptionHelper
+   */
+  public function __toJSON(): JSON {
+    return new JSON(json_encode($this));
+  }
+
+  /**
+   * @return array
+   * @throws ExceptionHelper
+   */
+  public function __toArray(): array {
+    return ($this->__toJSON())->__toArray();
+  }
+
+  /**
+   * @return string
+   * @throws ExceptionHelper
+   */
+  public function __toString(): string {
+    return (string)$this->__toJSON();
+  }
+}
