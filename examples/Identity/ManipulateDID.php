9f3129e85e3bc0327cc0d2bdee8141ec0963ba2c
SourCL <info@sourcl.com>
2021-08-10 12:29:12 +0200

    update examples Identity

--- examples/Identity/ManipulateDID.php
+++ examples/Identity/ManipulateDID.php
@@ -2,9 +2,11 @@
   // include iota lib
   require_once("../../autoload.php");
   // create DID
-  $identity = new \IOTA\Identity();
+  $identity = new \IOTA\Identity(null, 'b8abd06270050caa6d2643868a22ae081aa446c4271b271f61a04873c4eba097f4ea23602dbb40c511038e3e31a902b2eaea0e45542854dd6dc526caacf26eb9');
   $DID      = $identity->create();
+  // set Service
+  $service = new \IOTA\Identity\Service($DID->uri, 'LinkedDomains', 'test.IOTA.php');
   // manipulate DID
-  $manipulatedDID = $identity->manipulate($DID->document, $DID->messageId);
+  $manipulatedDID = $identity->manipulate($DID->document, $DID->messageId, $service , new \IOTA\Util\Keys());
   // output
   var_dump($manipulatedDID);
--- examples/Identity/VerifyDIDFromTangleMessageId.php
+++ examples/Identity/VerifyDIDFromTangleMessageId.php
@@ -2,6 +2,6 @@
   // include iota lib
   require_once("../../autoload.php");
   // verify TangleMessage
-  $result = \IOTA\Identity::verifyTangleMessage("a593cb3216a7571f2fa92065860c0e7da45ce8cbea3760084c45382a4d5cc5cc");
+  $result = \IOTA\Identity::verifyTangleMessage("1795ee882f61a51478723b5098ead9866ec4427ac1a4e74bc4a1e4d77c8e6a13");
   // output
   var_dump($result);
