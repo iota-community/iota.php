3d7113c79e0e04e3640b0000343dc937fe47cc6d
SourCL <info@sourcl.com>
2021-09-06 09:23:33 +0200

    update examples

--- examples/Identity/ManipulateDID.php
+++ examples/Identity/ManipulateDID.php
@@ -2,7 +2,7 @@
   // include iota lib
   require_once("../../autoload.php");
   // create DID
-  $identity = new \IOTA\Identity(null, 'b8abd06270050caa6d2643868a22ae081aa446c4271b271f61a04873c4eba097f4ea23602dbb40c511038e3e31a902b2eaea0e45542854dd6dc526caacf26eb9');
+  $identity = new \IOTA\Identity();
   $DID      = $identity->create();
   // set Service
   $service = new \IOTA\Identity\Service($DID->uri, 'LinkedDomains', 'test.IOTA.php');
