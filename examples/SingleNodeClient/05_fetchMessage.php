700778e56c142182e51d6ea489fc5ed62cd60f2f
SourCL <info@sourcl.com>
2022-02-07 10:59:23 +0100

    update example

--- examples/SingleNodeClient/05_fetchMessage.php
+++ examples/SingleNodeClient/05_fetchMessage.php
@@ -2,16 +2,24 @@
   // include iota lib
   require_once("../../autoload.php");
 
-  use IOTA\Client\SingleNodeClient;
-
   // create client
-  $client = new SingleNodeClient();
+  $client = new \IOTA\Client\SingleNodeClient();
   // find Message
   $found = $client->messagesFind('#iota.php');
   if(count($found->messageIds) > 0) {
     echo "Messages Found: " . count($found->messageIds) . PHP_EOL;
     $lastData = $client->message(end($found->messageIds));
-    echo $lastData->payload->data;
+
+    $data = "unknown";
+
+    if($lastData->payload instanceof \IOTA\Api\v1\ResponsePayloadTransaction) {
+      $data = $lastData->payload->essence->payload->data;
+    } else if($lastData->payload instanceof \IOTA\Api\v1\ResponsePayloadIndexation) {
+      $data = $lastData->payload->data;
+    }
+
+    echo \IOTA\Helper\Converter::hex2String($data);
+
   }
   else {
     echo "No Results!" . PHP_EOL;
